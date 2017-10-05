2b31e87dfc62cf9fcc41c849ba906b8aa596b2aa
HM Nayem <hm.nayem.95@gmail.com>
2017-10-05 07:47:23 +0600

    Minimum Final

--- js/app.js
+++ js/app.js
@@ -18,6 +18,11 @@ $(function(){
     $(ul).on('click', 'li', function(event){
         event.stopPropagation();
         $(this).find('p').toggleClass("selected");
+        if($(this).find('p').hasClass('selected')){
+            $(this).find(".check").attr('checked', true);
+        }else {
+            $(this).find(".check").attr('checked', false);
+        }
     }).on('mouseenter', 'li', function(){
         $(this).find('span').fadeIn(300);
         $(this).find('.check').fadeIn(300);
@@ -29,4 +34,25 @@ $(function(){
     $('#add-item').on('click', function(){
         $("#input-elements").slideToggle();
     });
+
+    $('#remove-items').on('click', function(event){
+       $(ul).find('.check:checked').parent().fadeOut(300, function(){
+           $(this).remove();
+       });
+    });
+
+    $('#input-elements').find('input[type=text]').on('keypress', function(event){
+        if(event.which == 13) {
+            addItem($(this).val());
+            $(this).val("");
+        }
+    });
+
+    function addItem(data) {
+        var text = '<li><span><i class="fa fa-trash" aria-hidden="true"></i></span>';
+        text += '<p>' + data + '</p>';
+        text += '<input type="checkbox" class="check"></li>';
+
+        $(ul).append(text);
+    }
 });
